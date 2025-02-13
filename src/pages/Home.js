ed6341bba763adef6641ac62a029eee96899b9dc
Nkechi <enugucats@gmail.com>
2025-02-13 12:02:20 +0100

    second commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,13 @@ import Services from "../components/Services";
 import About from "./About";
 import Pricing from "../components/Pricing";
 import Contact from "./Contact";
+import Profile from "../assets/images/003.jpg";
 
 
 const Home = () => {
   return (
     <div className="home-container">
-      <img className="top-right-image" src="../assets/images/003.pg" alt="Top Right" />
+      <img className="top-right-image" src= { Profile } alt="Top Right" />
       <div className="content">
         <h1>Welcome to Our Home Page!</h1>
         <p>This is an example of a homepage with a background image and an image on the top right corner.</p>
